49223198a32d5662581b770be2d398895b074259
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-06-08 15:27:51 +0530

    Add files via upload

---
+++ 100DaysOfPython/day05.py
@@ -0,0 +1,24 @@
+print('I am good boy \nI am good girl')
+# New design is coming
+# Author : AyusVaze67
+# Code : CrazyCodeAyush
+# sad
+# asd
+# as
+# dasd
+# print('Sentence') # print comment??
+# print('Hello \'Ayush\'')
+print('sdjk','asd',1,1,1,1,end='hello',sep='-')
+'''
+saddsadsadas
+dasd
+as
+d
+asd
+asd
+as
+das
+dasdasd
+asd
+asd
+'''
