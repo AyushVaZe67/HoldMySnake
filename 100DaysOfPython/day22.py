465cd646f7d55e1b492a8ed75234354df8be01d2
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-06-23 15:04:10 +0530

    Add files via upload

--- 100DaysOfPython/day22.py
+++ 100DaysOfPython/day22.py
@@ -4,4 +4,7 @@ list1 = [10,20,30,[1,2,3],40,50,60,70]
 # for i in range(len(list1)):
 #     print(list1[i])
 
-print(list1[0:14:2])
+# print(list1[0:14:2])
+
+list2 = [i for i in range(19) if i % 2 ==0]
+print(list2)
