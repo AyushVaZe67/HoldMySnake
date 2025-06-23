b2c2e75b457754e1ca76e3d10ebb02be0e59bda8
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-06-23 14:57:13 +0530

    Add files via upload

--- 100DaysOfPython/day22.py
+++ 100DaysOfPython/day22.py
@@ -1,5 +1,7 @@
-list1 = [10,20,30,[1,2,3]]
+list1 = [10,20,30,[1,2,3],40,50,60,70]
 # print(list1[3][1])
 
-for i in range(len(list1)):
-    print(list1[i])
+# for i in range(len(list1)):
+#     print(list1[i])
+
+print(list1[0:14:2])
