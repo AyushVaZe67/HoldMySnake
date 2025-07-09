265dead15031b38e6170f5ebc8fc862105ec0f68
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-09 17:22:44 +0530

    Add files via upload

--- MyPython/Practice/task-oops.py
+++ MyPython/Practice/task-oops.py
@@ -1,8 +1,18 @@
 class Student:
-    def __init__(self,full_name,roll_no):
+    def __init__(self,full_name,roll_no,percentage):
         self.name = full_name
+        self.percentage = percentage
         self.roll = roll_no
 
+    def details(self):
+        print(self.name , self.roll)
 
-student1 = Student('Ayush',17)
-print(student1.name,student1.roll)
+    def show_percentage(self):
+        print(f'{self.name} has {self.percentage}% with roll no {self.roll}')
+
+
+
+student1 = Student('Ayush',17,98)
+student2 = Student('Mia',18,69)
+student1.show_percentage()
+student2.show_percentage()
