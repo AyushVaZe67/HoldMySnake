96201e2c621de4f58218185f41b9927ff1c01916
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-12 21:51:38 +0530

    Add files via upload

--- 100DaysOfPython/day24.py
+++ 100DaysOfPython/day24.py
@@ -1,4 +1,11 @@
 t1 = (1,2,3,4,5,6,'red','green')
 # print(len(t1))
 # print(type(t1))
-print(t1)
+print(t1[-1])
+# for i in range(len(t1)):
+#     print(t1[i])
+
+if 1 not in t1:
+    print('YES')
+else:
+    print('NO')
