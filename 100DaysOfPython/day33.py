43473d737af6f4a545730fc8aa16ddba886179d6
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-13 14:45:01 +0530

    Add files via upload

--- 100DaysOfPython/day33.py
+++ 100DaysOfPython/day33.py
@@ -1,2 +1,9 @@
 d1 = {'9':'Ayush','10':'Mia','11':'Dani'}
-print(d1['10'])
+# print(d1['10'])
+# print(d1.values())
+# print(d1.keys())
+# print(d1.items())
+
+for key,value in d1.items():
+    print(key,value)
+
