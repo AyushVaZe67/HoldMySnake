210e0085ffb5b6f24f2a7c4cb70177b68e5b3894
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-14 19:23:36 +0530

    Add files via upload

--- 100DaysOfPython/day52.py
+++ 100DaysOfPython/day52.py
@@ -1,2 +1,10 @@
-x = int(input('Enter num:'))
-print((lambda x: x ** 2)(5))
+# x = int(input('Enter num:'))
+# print((lambda x: x ** 2)(5))
+# print((lambda x,y : (x+y)/2)(5,9))
+
+avg = (lambda x,y : (x+y)/2)
+
+def f1(avg1,value):
+    return avg1 + value
+
+print(f1(avg(5,7),10))
