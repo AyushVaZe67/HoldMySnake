ee49fa1c6669198b5c6a5bcd892d126ca22143f3
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-14 21:55:30 +0530

    Add files via upload

--- 100DaysOfPython/day55.py
+++ 100DaysOfPython/day55.py
@@ -1,3 +1,22 @@
-import random
+import random as r
 
-user_input = int(input('Enter '))
+selector = ["Snake", "Water", "Gun"]
+player_choice = input("Snake, Water, or Gun?: ")
+
+r_matrix = [
+    ["Draw", "Win", "Lose"],
+    ["Lose", "Draw", "Win"],
+    ["Win", "Lose", "Draw"]
+]
+
+if player_choice not in selector:
+    print('Invalid Input')
+else:
+    machine_choice = r.choice(selector)
+    print("Machine Selected: ", machine_choice)
+
+    p_index = selector.index(player_choice)
+    m_index = selector.index(machine_choice)
+
+    result = r_matrix[p_index][m_index]
+    print(result)
