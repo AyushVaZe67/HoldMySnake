f89db41bf692a69201fdef030049aae19554ee88
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-14 19:15:06 +0530

    Add files via upload

--- 100DaysOfPython/day51.py
+++ 100DaysOfPython/day51.py
@@ -2,5 +2,8 @@ with open('myfile.txt','r') as f:
     print(type(f))
     f.seek(5)
 
-    data = f.read(15)
+    data = f.read(11)
+    print(data)
+    print(f.tell())
+    f.truncate(5)
     print(data)
