9b05462110be4fc15ae18d81a41a13e5795ea0f7
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-14 21:25:48 +0530

    Add files via upload

--- 100DaysOfPython/day53.py
+++ 100DaysOfPython/day53.py
@@ -1,9 +1,14 @@
 def cube(x):
     return x ** 3
 
+def filter_function(a):
+    return a > 2
+
 l1 = [1,2,3,4,5]
 
 # for i in range(len(l1)):
 #     print(cube(l1[i]))
 
 print(list(map(cube,l1)))
+
+print(list(filter(filter_function,l1)))
