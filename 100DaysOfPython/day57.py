c1580bf764dbbe6022388be237c9fb52582c2859
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-15 13:20:47 +0530

    Add files via upload

--- 100DaysOfPython/day57.py
+++ 100DaysOfPython/day57.py
@@ -3,6 +3,15 @@ class Person:
     occupation = 'Student'
     age = 21
 
+    def info(self):
+        print(f'{self.name} is {self.occupation}')
+
 a = Person()
 a.name = 'Mia'
-print(a.name)
+# print(a.name)
+a.info()
+
+b = Person()
+b.name = 'Alex'
+b.occupation = 'HR'
+b.info()
