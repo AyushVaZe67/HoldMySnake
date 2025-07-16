2448fc165bed6caccc88d95db09b983dc5a7054c
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-16 15:35:20 +0530

    Add files via upload

--- 100DaysOfPython/day64.py
+++ 100DaysOfPython/day64.py
@@ -2,12 +2,13 @@ class Library:
     books = []
     no_of_books = 0
 
-    def add_book(self,book_name):
-        Library.books.append(book_name)
-        self.no_of_books += 1
+    def add_book(self, book_name):
+        self.__class__.books.append(book_name)
+        self.__class__.no_of_books += 1
 
     def show_all_books(self):
-        print(self.books)
+        print("Books in library:", self.__class__.books)
+        print("Total books:", self.__class__.no_of_books)
 
 l1 = Library()
 l1.add_book('Bible')
