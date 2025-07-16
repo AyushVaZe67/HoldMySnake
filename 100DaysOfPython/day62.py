366de351a8e86fde354b1d041343db675dc96c04
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-16 14:00:11 +0530

    Add files via upload

--- 100DaysOfPython/day62.py
+++ 100DaysOfPython/day62.py
@@ -1,8 +1,13 @@
 class E1:
-    def __init__(self,name):
+    def __init__(self,name,age):
         self.__name = name
+        self._age = age
 
+class E2(E1):
+    def showAge(self):
+        print(f'{self._age}')
 
-e1 = E1('Ayush')
+e1 = E1('Ayush',22)
 print(e1._E1__name)
-
+e2 = E2('Mia',32)
+print(e2._age)
