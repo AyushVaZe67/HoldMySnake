65433493dc65f3d098038ba0a2aa5124c42b2b9d
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-16 13:51:23 +0530

    Add files via upload

--- 100DaysOfPython/day61.py
+++ 100DaysOfPython/day61.py
@@ -6,6 +6,11 @@ class Employee:
     def showDetails(self):
         print(f'Name: {self.name} and id: {self.id}')
 
+class Programmer(Employee):
+    def showLanguage(self):
+        print('The default language is Python')
 
 e = Employee('Ayush',12)
 e.showDetails()
+e2 = Programmer('Ayush',45)
+e2.showLanguage()
