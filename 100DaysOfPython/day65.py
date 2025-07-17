f7d6ee2974e4a2d06a3d87fa3765e7b76c73c25f
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-17 10:55:04 +0530

    Add files via upload

---
+++ 100DaysOfPython/day65.py
@@ -0,0 +1,10 @@
+class Math:
+    def __init__(self,num):
+        self.num = num
+
+    def addtonum(self,n):
+        self.num = self.num + n
+
+    @staticmethod
+    def add(a,b):
+        return a + b
