5d0e7e30cd8022c8cd0d66967d4f4f5e397b04aa
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-17 11:32:27 +0530

    Add files via upload

--- 100DaysOfPython/day66.py
+++ 100DaysOfPython/day66.py
@@ -1,10 +1,15 @@
 class Employee:
+    company_name = 'Google'
     def __init__(self,name):
         self.name = name
+        self.raise_amount = 0.02
 
     def showDetails(self):
-        print(self.name)
+        print(self.name , self.raise_amount, self.company_name)
 
 emp1 = Employee('Ayush')
-emp1.name = 'Ayush1'
 emp1.showDetails()
+
+emp2 = Employee('Mia')
+emp2.raise_amount = 0.01
+emp2.showDetails()
