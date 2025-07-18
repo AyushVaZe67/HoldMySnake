b35d77223cfaa602c4e24edcb53e0a4d792aaebf
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-18 10:54:34 +0530

    Add files via upload

--- 100DaysOfPython/day71.py
+++ 100DaysOfPython/day71.py
@@ -1,3 +1,13 @@
 x = [12,34,56]
 # print(dir(x))
 # print(x.__add__)
+class Person:
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+        self.age = 22
+
+p1 = Person('Ayush',12000)
+# print(p1.__dict__)
+
+print(help(Person))
