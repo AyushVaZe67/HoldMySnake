f12f3714ab9ce9ea2014eb0e195b4baa5357143c
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-18 10:44:59 +0530

    Add files via upload

---
+++ 100DaysOfPython/day70.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
+
+    @classmethod
+    def fromStr(cls,string):
+        return cls(s1.split('-')[0],s1.split('-')[1])
+
+
+
+e1 = Person('Ayush',12345)
+print(e1.name)
+print(e1.salary)
+
+s1 = 'Mia-12000'
+e2 = Person.fromStr(s1)
+print(e2.name)
+print(e2.salary)
