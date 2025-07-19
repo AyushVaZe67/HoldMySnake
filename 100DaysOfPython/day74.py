dbd06503ea320280d8fec6427395c58833a9db04
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-19 14:08:07 +0530

    Add files via upload

--- 100DaysOfPython/day74.py
+++ 100DaysOfPython/day74.py
@@ -6,7 +6,18 @@ class Shape:
     def area(self):
         return self.x * self.y
 
+class Circle:
+    def __init__(self,x):
+        self.x = x
+
+    def area(self):
+        return 3.1415 * super().area()
+
+
 
 
 s1 = Shape(12,12)
 print(s1.area())
+
+c1 = Circle(12)
+print(c1.area())
