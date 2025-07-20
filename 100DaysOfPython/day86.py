55bc3231275cd6c5143382ba59a3fbafaaf51201
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-20 15:55:24 +0530

    Add files via upload

--- 100DaysOfPython/day86.py
+++ 100DaysOfPython/day86.py
@@ -1,6 +1,11 @@
 # a = True
 # print(a:=False)
 
-num = [1,2,3,4,5]
-while (n := len(num)) > 0:
-    print(num.pop())
+# num = [1,2,3,4,5]
+# while (n := len(num)) > 0:
+#     print(num.pop())
+#
+
+foods = []
+while(food := input('Enter food: ')) != 'quit':
+    foods.append(food)
