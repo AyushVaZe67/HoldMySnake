db4568c686f3c697114681b7966267c388319ff0
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-22 13:26:08 +0530

    Add files via upload

--- 100DaysOfPython/day91.py
+++ 100DaysOfPython/day91.py
@@ -1,9 +1,10 @@
 def generator():
-    for i in range(115):
+    for i in range(1,21):
         yield i
 
 
 gen = generator()
 
 for j in gen:
-    print(j,end=' ')
+    if j % 2 == 0:
+        print(j,end=' ')
