931e7c39aead3543cb4de9b213700dc963942631
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-23 21:34:39 +0530

    Add files via upload

--- 100DaysOfPython/day97.py
+++ 100DaysOfPython/day97.py
@@ -5,6 +5,25 @@ def func1(s):
     print(f'Sleeping for {s} seconds')
     time.sleep(s)
 
-func1(4)
-func1(4)
-func1(4)
+# time1 = time.perf_counter()
+# func1(4)
+# func1(4)
+# func1(4)
+# time2 = time.perf_counter()
+# print(time2-time1)
+
+time1 = time.perf_counter()
+t1 = threading.Thread(target=func1, args=[3])
+t2 = threading.Thread(target=func1, args=[2])
+t3 = threading.Thread(target=func1, args=[4])
+
+t1.start()
+t2.start()
+t3.start()
+
+t1.join()
+t2.join()
+t3.join()
+
+time2 = time.perf_counter()
+print(f'\n{time2-time1}')
