d114d1b762f2832ee346268c3074ee768587cbf8
Ayush VaZe <145156595+AyushVaZe67@users.noreply.github.com>
2025-07-23 22:17:58 +0530

    Add files via upload

--- 100DaysOfPython/day99.py
+++ 100DaysOfPython/day99.py
@@ -1,3 +1,22 @@
-#solution to drink water
 import time
 from plyer import notification
+import winsound
+import os
+
+sound_path = "C:\\Windows\\Media\\Windows Notify System Generic.wav"
+
+while True:
+    time.sleep(5)
+
+    # Show Notification
+    notification.notify(
+        title='Ayush',
+        message='Drink Water',
+        timeout=2
+    )
+
+    # Play Sound
+    if os.path.exists(sound_path):
+        winsound.PlaySound(sound_path, winsound.SND_FILENAME)
+    else:
+        print("Sound file not found!")
